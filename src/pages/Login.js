174f0c4b3cb57fc0ec9f4e205c601611330666b1
vijay subramanian <vijaysubbu69@gmail.com>
2023-11-19 14:10:50 +0530

    demo credential added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,45 +4,64 @@ import { Link } from "react-router-dom";
 import { useLoginMutation } from "../services/appApi";
 
 function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [login, { isError, isLoading, error }] = useLoginMutation();
-    function handleLogin(e) {
-        e.preventDefault();
-        login({ email, password });
-    }
-    return (
-        <Container>
-            <Row>
-                <Col md={6} className="login__form--container">
-                    <Form style={{ width: "100%" }} onSubmit={handleLogin}>
-                        <h1>Login to your account</h1>
-                        {isError && <Alert variant="danger">{error.data}</Alert>}
-                        <Form.Group>
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" value={email} required onChange={(e) => setEmail(e.target.value)} />
-                        </Form.Group>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, { isError, isLoading, error }] = useLoginMutation();
+  function handleLogin(e) {
+    e.preventDefault();
+    login({ email, password });
+  }
+  return (
+    <Container>
+      <Row>
+        <Col md={6} className="login__form--container">
+          <Form style={{ width: "100%" }} onSubmit={handleLogin}>
+            <h1>Login to your account</h1>
+            {isError && <Alert variant="danger">{error.data}</Alert>}
+            <Form.Group>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
 
-                        <Form.Group className="mb-3">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Enter Password" value={password} required onChange={(e) => setPassword(e.target.value)} />
-                        </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter Password"
+                value={password}
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
 
-                        <Form.Group>
-                            <Button type="submit" disabled={isLoading}>
-                                Login
-                            </Button>
-                        </Form.Group>
+            <Form.Group>
+              <Button type="submit" disabled={isLoading}>
+                Login
+              </Button>
+            </Form.Group>
 
-                        <p className="pt-3 text-center">
-                            Don't have an account? <Link to="/signup">Create account</Link>{" "}
-                        </p>
-                    </Form>
-                </Col>
-                <Col md={6} className="login__image--container"></Col>
-            </Row>
-        </Container>
-    );
+            <p className="pt-3 text-center">
+              Don't have an account? <Link to="/signup">Create account</Link>{" "}
+            </p>
+            <br />
+            <p className="pt-3 text-center">
+              USER LOGIN: demouser@gmail.com & PWD: demo123
+            </p>
+            <p className="pt-3 text-center">
+              ADMIN LOGIN : adminuser@gmail.com & PWD: admin123
+            </p>
+          </Form>
+        </Col>
+        <Col md={6} className="login__image--container"></Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Login;
